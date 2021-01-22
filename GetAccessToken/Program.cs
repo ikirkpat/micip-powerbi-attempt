336531546281b87d6127ab91487c8db58be1594f
Ian Kirkpatrick <ikirkpat@oaisd.org>
2021-01-22 08:53:29 -0500

    Add script for fetching the access token

---
+++ GetAccessToken/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using static GetAccessToken.MicrosoftIdentityTokenRetriever;
+
+namespace GetAccessToken
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      var config = MiDataHubReportsConfiguration.GetDefaultConfiguration();
+      var tokenRetriever = new MicrosoftIdentityTokenRetriever(config);
+      string accessToken = tokenRetriever.GetAccessToken().Result;
+      Console.WriteLine("AccessToken:\n\n" + accessToken);
+    }
+  }
+
+  /// <summary>
+  /// Provides ability to fetch an access token from a Microsoft Identity Provider.
+  /// This instances underlying provider is a Microsoft Identity Provider.
+  /// </summary>
+  public class MicrosoftIdentityTokenRetriever
+  {
+    private MiDataHubReportsConfiguration configuration;
+
+    public MicrosoftIdentityTokenRetriever(MiDataHubReportsConfiguration configuration)
+    {
+      this.configuration = configuration;
+    }
+
+    /// <inheritdoc/>
+    public async Task<string> GetAccessToken()
+    {
+      AuthenticationResult authenticationResult = null;
+      // Create a public client to authorize the app with the AAD app
+      IPublicClientApplication clientApp = PublicClientApplicationBuilder.Create(this.configuration.PbiClientId)
+                                                                         .WithAuthority(this.configuration.PbiAuthorityUri)
+                                                                         .Build();
+      var scopes = new string[] { this.configuration.PbiAuthenticationScope };
+      IEnumerable<IAccount> userAccounts = await clientApp.GetAccountsAsync();
+      try
+      {
+        // Retrieve Access token from cache if available
+        authenticationResult = await clientApp.AcquireTokenSilent(scopes, userAccounts.FirstOrDefault()).ExecuteAsync();
+      }
+      catch (MsalUiRequiredException)
+      {
+        SecureString securePassword = new SecureString();
+        foreach (char passwordChar in this.configuration.PbiPassword)
+        {
+          securePassword.AppendChar(passwordChar);
+        }
+        authenticationResult = await clientApp.AcquireTokenByUsernamePassword(
+          scopes,
+          this.configuration.PbiUsername,
+          securePassword)
+          .ExecuteAsync();
+      }
+      return authenticationResult.AccessToken;
+    }
+
+    public class MiDataHubReportsConfiguration
+    {
+      public string PbiUsername { get; set; } = string.Empty;
+      public string PbiPassword { get; set; } = string.Empty;
+      public string PbiAuthenticationScope { get; set; } = string.Empty;
+      public string PbiClientId { get; set; } = string.Empty;
+      public string PbiAuthorityUri { get; set; } = string.Empty;
+
+      public static MiDataHubReportsConfiguration GetDefaultConfiguration()
+      {
+        return new MiDataHubReportsConfiguration()
+        {
+          PbiUsername = "SA_MiDatahub-PowerBI@michigan.gov",
+          PbiPassword = "SYtrp1G4Z!YbJt6bnKoM5%MPe",
+          PbiClientId = "7d3f3c68-21cd-490c-9484-b9b097b0bc23",
+          PbiAuthorityUri = "https://login.microsoftonline.com/organizations/",
+          PbiAuthenticationScope = "https://analysis.usgovcloudapi.net/powerbi/api/.default"
+        };
+      }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Open index.html in your browser to see the demo. Go to each of the links on it to look at each problem I'm having.
 
-This evening, I'll try to get a better way of getting the access token. For now, you'll have to get it using a different app and just plug it into this web page before you click embed.
+When you click to embed each time, you'll need an access token. fetch this by running the GetAccessToken project in this repository. It's a .NET Core Console app. All you need to do is call `dotnet run` in that project and it will give you an access token. Copy/Paste it into the input on the web page before you run the embed.
